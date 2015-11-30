069ba89713ec6513c2d67478af4fe8bffea9247e
sindain <sindain89@hotmail.com>
2015-11-30 16:32:57 -0600

    transfer

--- NeonHell/Transfer/Aaron/Scripts/ThrusterController.cs
+++ NeonHell/Transfer/Aaron/Scripts/ThrusterController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ThrusterController : MonoBehaviour {
 
-	public float fMaxG = 10.0f;
+	public float fMaxG = 5.0f;
 
 	//Private variables
 	private int iThrusterCount;
@@ -21,9 +21,9 @@ public class ThrusterController : MonoBehaviour {
 			thrusters [i] = transform.FindChild ("Thrusters").GetChild (i);
 		fThrustStrength = (-Physics.gravity.y * rb.mass) / iThrusterCount;
 		if (GetComponent<PlayerController> () != null)
-			fThrustDistance = GetComponent<PlayerController> ().getAirborneDistance () / 2.0f;
+			fThrustDistance = GetComponent<PlayerController> ().getAirborneDistance ()/2.0f;
 		else
-			fThrustDistance = GetComponent<NPCController> ().getAirborneDistance () / 2.0f;
+			fThrustDistance = GetComponent<NPCController> ().getAirborneDistance ()/2.0f;
 	}
 
 	void Awake(){
@@ -46,12 +46,16 @@ public class ThrusterController : MonoBehaviour {
 				//Check to make sure the object hit wasn't a trigger
 				if(hit.collider.isTrigger)
 					return;
-				
-		        if (hit.distance < fThrustDistance)
-		          //distancePercentage = -(fMaxG - 1) / fThrustDistance * hit.distance + fMaxG;
-		          distancePercentage = ((fMaxG - 1)/Mathf.Pow(fThrustDistance,2)) * Mathf.Pow(hit.distance - fThrustDistance,2) + 1;
-		        else
-		          distancePercentage = -1 / fThrustDistance * hit.distance + 2;
+
+				if (hit.distance < fThrustDistance){
+					distancePercentage = -(fMaxG - 1) / fThrustDistance * hit.distance + fMaxG;
+					distancePercentage += -rb.GetPointVelocity(i.position).y;
+					//distancePercentage = ((fMaxG - 1)/Mathf.Pow(fThrustDistance,2)) * Mathf.Pow(hit.distance - fThrustDistance,2) + 1;
+
+				}
+				else
+					distancePercentage = -1 / fThrustDistance * hit.distance + 2;
+				//distancePercentage = Mathf.Abs(distancePercentage);
 				print(distancePercentage);
 				//float x = rb.GetPointVelocity(i.position).y < 0? iThrustPercent + iThrustPercent * -rb.GetPointVelocity(i.position).y : iThrustPercent;
 				//Do some math to calculate the thruster strength and apply it to the ships rigid body
