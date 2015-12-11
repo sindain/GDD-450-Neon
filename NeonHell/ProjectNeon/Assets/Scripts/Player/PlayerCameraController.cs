2e5b329c26c2a00e6c8f07a27e5c6c234bf13099
sindain <sindain89@hotmail.com>
2015-12-11 15:46:24 -0600

    ... forgot the camera script

--- NeonHell/ProjectNeon/Assets/Scripts/Player/PlayerCameraController.cs
+++ NeonHell/ProjectNeon/Assets/Scripts/Player/PlayerCameraController.cs
@@ -2,22 +2,13 @@
 using System.Collections;
 
 public class PlayerCameraController : MonoBehaviour {
-	public Camera playerCamera;
 
-	public float rotSpeed 		= 750.0f;
+	public float fSlerpTime	= 0.333f;
+	public Camera playerCamera;
 	public Vector3 vCameraOffset;
-	private float rotVelocity 	= 0.0f;
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 newPos = transform.position + transform.rotation * vCameraOffset;
-		Vector3 newRot = new Vector3(transform.eulerAngles.x+15.0f, transform.eulerAngles.y, transform.eulerAngles.z);
-		//newRot.x = Mathf.SmoothDampAngle(newRot.x, transform.eulerAngles.x + 15.0f, ref rotVelocity, rotSpeed);
-		playerCamera.transform.eulerAngles = newRot;
-		playerCamera.transform.position = newPos;
+	void FixedUpdate(){
+		playerCamera.transform.position = Vector3.Slerp(playerCamera.transform.position, transform.position + transform.rotation * vCameraOffset, fSlerpTime);
+		playerCamera.transform.rotation = Quaternion.Slerp(playerCamera.transform.rotation, transform.rotation, fSlerpTime);
 	}
 }
