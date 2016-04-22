e41637480d2926d3bb40df43733bab832c6a745c
jonyboy199415 <jonnyboy199413@yahoo.com>
2016-04-21 22:39:11 -0500

    started work on this need a bit of help to finish

--- NeonHell/ProjectNeon/Assets/Scripts/BridgeTrigger.cs
+++ NeonHell/ProjectNeon/Assets/Scripts/BridgeTrigger.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class BridgeTrigger : MonoBehaviour {
-
+public class BridgeTrigger : NetworkBehaviour {
+	public GameObject Bridge;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	[Command]
+	public void CmdBridgeOff(){
+		Bridge.gameObject.SetActive (false);
+		RpcBridgeOff();
+	}
+	[ClientRpc]
+	public void RpcBridgeOff(){
+		Bridge.gameObject.SetActive (false);
+	}
+
+	[Command]
+	public void CmdBridgeOn(){
+		Bridge.gameObject.SetActive (true);
+		RpcBridgeOn ();
+	}
+	[ClientRpc]
+	public void RpcBridgeOn(){
+		Bridge.gameObject.SetActive (true);
 	}
 }
