cee1d61cc531efe0b1d21147914bdd433757ee34
CausticTarget <grantbordson@gmail.com>
2016-04-25 18:14:52 -0500

    fix to random song

--- NeonHell/ProjectNeon/Assets/Scripts/MusicRanScript.cs
+++ NeonHell/ProjectNeon/Assets/Scripts/MusicRanScript.cs
@@ -5,7 +5,7 @@ public class MusicRanScript : MonoBehaviour {
 	public AudioClip[] MusicTracks;
 	// Use this for initialization
 	void Start () {
-		gameObject.GetComponent<AudioSource> ().clip = MusicTracks [Random.Range (0, MusicTracks.Length)];
+		gameObject.GetComponent<AudioSource> ().clip = MusicTracks [Random.Range (0, MusicTracks.Length-1)];
 		gameObject.GetComponent<AudioSource> ().PlayOneShot (gameObject.GetComponent<AudioSource> ().clip);
 	}
 	
