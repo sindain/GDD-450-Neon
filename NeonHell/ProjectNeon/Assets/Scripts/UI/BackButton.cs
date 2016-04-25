b23a8c17cf1b69423376bab645dc11b46652e4a9
jonyboy199415 <jonnyboy199413@yahoo.com>
2016-04-25 01:07:40 -0500

    added a buntch of stuf to get the last city working. forgot to push up earlier.

--- NeonHell/ProjectNeon/Assets/Scripts/UI/BackButton.cs
+++ NeonHell/ProjectNeon/Assets/Scripts/UI/BackButton.cs
@@ -3,8 +3,56 @@ using System.Collections;
 
 public class BackButton : MonoBehaviour {
   public GameObject returnObject;
+	public GameObject FirstPage;
+	public GameObject SecondPage;
+	public GameObject ThirdPage;
+	private int pagecounter=0;
+
+
   public void clicked() {
     returnObject.SetActive(true);
     gameObject.SetActive(false);
   }
+	public void Nxtclicked() {
+		pagecounter++;
+		if (pagecounter > 2) {
+			pagecounter = 0;
+		}
+		if (pagecounter == 0) {
+			FirstPage.SetActive (true);
+			SecondPage.SetActive (false);
+			ThirdPage.SetActive (false);
+		}
+		if (pagecounter == 1) {
+			FirstPage.SetActive (false);
+			SecondPage.SetActive (true);
+			ThirdPage.SetActive (false);
+		}
+		if (pagecounter == 2) {
+			FirstPage.SetActive (false);
+			SecondPage.SetActive (false);
+			ThirdPage.SetActive (true);
+		}
+	}public void Bckclicked() {
+		pagecounter--;
+		if (pagecounter < 0) {
+			pagecounter = 2;
+		}
+		if (pagecounter == 0) {
+			FirstPage.SetActive (true);
+			SecondPage.SetActive (false);
+			ThirdPage.SetActive (false);
+		}
+		if (pagecounter == 1) {
+			FirstPage.SetActive (false);
+			SecondPage.SetActive (true);
+			ThirdPage.SetActive (false);
+		}
+		if (pagecounter == 2) {
+			FirstPage.SetActive (false);
+			SecondPage.SetActive (false);
+			ThirdPage.SetActive (true);
+		}
+	}
+
 }
